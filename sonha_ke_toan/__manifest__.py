d6583f26aaa16784d463a34c14901c735c37fb68
Phạm Văn Sơn <sonpv@sonha.com.vn>
2025-12-29 14:20:18 +0700

    update code Phiếu báo nợ

--- sonha_ke_toan/__manifest__.py
+++ sonha_ke_toan/__manifest__.py
@@ -49,6 +49,7 @@
         'views/acc_bao_cao_views.xml',
         'views/nl_acc_bao_cao_views.xml',
         'views/acc_kchuyen_ct_views.xml',
+        'views/nl_acc_ap_h_bn_views.xml',
         'wizard/thong_bao_wizard_views.xml',
         'wizard/popup_tim_kiem_views.xml',
         'wizard/popup_bao_cao_views.xml',
