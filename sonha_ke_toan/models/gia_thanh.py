de01e129933e8805621877d39e0d06fba447b54a
sonhasoftware <sonha.software.1@gmail.com>
2026-08-12 14:02:02 +0700

    update code

---
+++ sonha_ke_toan/models/gia_thanh.py
@@ -0,0 +1,63 @@
+from odoo import models, fields
+
+
+class GiaThanh(models.Model):
+    _name = 'gia.thanh'
+    _description = 'Giá thành'
+
+    nhom = fields.Char(string='Nhóm')
+
+    tk_auto = fields.Boolean(string='TK Auto',default=False)
+
+    thang = fields.Integer(string='Tháng')
+
+    nam = fields.Integer(string='Năm')
+
+    dg_theo_tien = fields.Boolean(string='ĐG theo tiền',default=False)
+
+    kc_bo_phan = fields.Boolean(string='Bộ phận',default=False)
+
+    kc_vu_viec = fields.Boolean(string='Vụ việc',default=False)
+
+    kc_khach_hang = fields.Boolean(string='Khách hàng',default=False)
+
+    kc_tscd = fields.Boolean(string='TSCĐ',default=False)
+
+    kc_khoan_muc = fields.Boolean(string='Khoản mục',default=False)
+
+    kc_phan_xuong = fields.Boolean(string='Phân xưởng',default=False)
+
+    kc_dtcp_tp = fields.Boolean(string='DTCP TP', default=False)
+
+    kc_vlsphh = fields.Boolean(string='VLSPHH',default=False)
+
+    kc_kho = fields.Boolean(string='Kho',default=False)
+
+    pb_bo_phan = fields.Boolean(string='Bộ phận',default=False)
+
+    pb_vu_viec = fields.Boolean(string='Vụ việc',default=False)
+
+    pb_khach_hang = fields.Boolean(string='Khách hàng',default=False)
+
+    pb_tscd = fields.Boolean(string='TSCĐ',default=False)
+
+    pb_khoan_muc = fields.Boolean(string='Khoản mục',default=False)
+
+    pb_phan_xuong = fields.Boolean(string='Phân xưởng',default=False)
+
+    pb_dtcp_tp = fields.Boolean(string='DTCP TP',default=False)
+
+    pb_vlsphh = fields.Boolean(string='VLSPHH',default=False)
+
+    pb_kho = fields.Boolean(string='Kho',default=False)
+
+
+    tieu_thuc_so_luong = fields.Boolean(
+        string='Số lượng',
+        default=False
+    )
+
+    tieu_thuc_tien = fields.Boolean(
+        string='Tiền',
+        default=False
+    )
