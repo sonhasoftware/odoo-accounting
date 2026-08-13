aa924662bbe4d0830639d604f9c292bbbe78640a
sonhasoftware <sonha.software.1@gmail.com>
2026-08-13 11:54:00 +0700

    Force logout users when login lock is active

--- sonha_phan_quyen/controllers/session.py
+++ sonha_phan_quyen/controllers/session.py
@@ -6,4 +6,18 @@ class SonhaSessionController(http.Controller):
 
     @http.route('/sonha/session/is_alive', type='json', auth='public', csrf=False)
     def is_alive(self):
-        return {'is_alive': bool(request.session.uid)}
+        uid = request.session.uid
+        if not uid:
+            return {'is_alive': False}
+
+        env = request.env(user=uid)
+        user = env['res.users'].sudo().browse(uid)
+        login_locked = env['ir.config_parameter'].sudo().get_param(
+            user.SONHA_LOGIN_LOCK_PARAM
+        ) == '1'
+
+        if login_locked and user.exists() and not user.has_group('base.group_system'):
+            request.session.logout(keep_db=True)
+            return {'is_alive': False}
+
+        return {'is_alive': True}
